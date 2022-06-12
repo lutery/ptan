80cd853c699a5562d01bf4519d6bd96820f8b47a
lutery <lutery@foxmail.com>
2022-06-12 17:21:03 +0800

    Update actions.py
    完善选择器的注释

--- ptan/actions.py
+++ ptan/actions.py
@@ -30,12 +30,19 @@ class EpsilonGreedyActionSelector(ActionSelector):
         # 根据每个动作的q值选择将要执行的动作
         # 再根据epsilon决定是否需要将动作用随机动作替代
         # 最后返回选择的动作
-        # parma scores: q值 维度是可能都所执行的每一个动作带来的q值，多维
+        # parma scores: q值 shape(batch_size, actions)
         assert isinstance(scores, np.ndarray)
         batch_size, n_actions = scores.shape
+        # actions shape is （batch_size, 1），其中1表示所选择动作索引
         actions = self.selector(scores)
+        # 下面的对比操作，所有小于sel.epsilon的都为True，所以大于self.epsilon都为true
+        # 结果会类似于[true false false...]
+        # 意义：在本次批量训练的样本中，随机选择一些样本使用随机探索的方式进行执行动作选择
         mask = np.random.random(size=batch_size) < self.epsilon
+        # sum(mask)统计有多少个动作是随机选择
+        # np.random.choice表示每个需要随机的动作选择所执行的动作的索引
         rand_actions = np.random.choice(n_actions, sum(mask))
+        # 使用这种方式赋值，可以保证仅赋值给为True索引的数组，
         actions[mask] = rand_actions
         return actions
 
