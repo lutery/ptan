dee0fc6ced446053cd6af6a8d2160ae72f485037
lutery <lutery@foxmail.com>
2023-11-04 20:02:01 +0800

    Update utils.py
    完成RewardTracker注释

--- ptan/common/utils.py
+++ ptan/common/utils.py
@@ -350,36 +350,43 @@ class RewardTracker:
         """
         Constructs RewardTracker
         :param writer: writer to use for writing stats,传入SummaryWriter类型，用来跟踪训练中的奖励
-        :param min_ts_diff: minimal time difference to track speed 最小的时间差跟踪速度 todo  作用
+        :param min_ts_diff: minimal time difference to track speed 最小的时间差跟踪速度，奖励记录是仅记录一个时间段的奖励值，当当前时间到达这个时间差min_ts_diff后，就会重新记录起始时间以及当前进行的帧数
         """
         self.writer = writer
         self.min_ts_diff = min_ts_diff
 
     def __enter__(self):
+        '''
+        with as 语法开始时会执行这里
+        '''
         self.ts = time.time()
-        self.ts_frame = 0
-        self.total_rewards = []
+        self.ts_frame = 0 # 记录上次时间
+        self.total_rewards = [] # 清空目前为止所有得到的奖励
         return self
 
     def __exit__(self, *args):
         self.writer.close()
 
     def reward(self, reward, frame, epsilon=None):
-        self.total_rewards.append(reward)
-        mean_reward = np.mean(self.total_rewards[-100:])
-        ts_diff = time.time() - self.ts
-        if ts_diff > self.min_ts_diff:
-            speed = (frame - self.ts_frame) / ts_diff
-            self.ts_frame = frame
-            self.ts = time.time()
-            epsilon_str = "" if epsilon is None else ", eps %.2f" % epsilon
+        '''
+        param reward: 奖励
+        param frame: 训练过程中总共进行的帧数
+        '''
+        self.total_rewards.append(reward) # 存储奖励
+        mean_reward = np.mean(self.total_rewards[-100:]) # 记录最新的100个奖励的平均值
+        ts_diff = time.time() - self.ts # 计算上次重置时间开始经过的的时间差
+        if ts_diff > self.min_ts_diff: # 如果时间差超过了指定的大小，
+            speed = (frame - self.ts_frame) / ts_diff # 计算当前时间段类的进行的帧数，用来计算游戏的速度
+            self.ts_frame = frame # 重置起始第几帧
+            self.ts = time.time() # 重置起始记录时间
+            epsilon_str = "" if epsilon is None else ", eps %.2f" % epsilon # todo 作用
             print("%d: done %d episodes, mean reward %.3f, speed %.2f f/s%s" % (
                 frame, len(self.total_rewards), mean_reward, speed, epsilon_str
-            ))
+            )) # 打印帧数、记录的总奖励个数，最新100次游戏的平均奖励，游戏进行的速度，todo epsilon的作用
             sys.stdout.flush()
-            self.writer.add_scalar("speed", speed, frame)
+            self.writer.add_scalar("speed", speed, frame) # 记录速度
         if epsilon is not None:
-            self.writer.add_scalar("epsilon", epsilon, frame)
-        self.writer.add_scalar("reward_100", mean_reward, frame)
-        self.writer.add_scalar("reward", reward, frame)
-        return mean_reward if len(self.total_rewards) > 30 else None
+            self.writer.add_scalar("epsilon", epsilon, frame) # 记录epsilon
+        self.writer.add_scalar("reward_100", mean_reward, frame) # 记录最近100游戏的平均奖励
+        self.writer.add_scalar("reward", reward, frame) # 记录最新一次游戏的奖励
+        return mean_reward if len(self.total_rewards) > 30 else None # 如果记录的总奖励数超过30个，则返回平均奖励值否则返回None
